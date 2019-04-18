4dc75bc0dd3c148c544890466568f1bd361cf51d
bC933 <yangxiongsen@126.com>
2019-04-18 10:35:09 +0800

    ver1.1

--- src/com/ui/ClientUI.java
+++ src/com/ui/ClientUI.java
@@ -9,7 +9,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
